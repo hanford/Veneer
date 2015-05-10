71d276bce8f92db583279f0e620b37bde80df9ef
jack hanford <jackhanford@gmail.com>
2015-05-09 20:50:54 -0700

    using native fetch, added item removing

--- app/js/export.js
+++ app/js/export.js
@@ -1,18 +1,43 @@
-var list = document.querySelector('.list');
+var list = document.querySelector('.list'), saved;
 
-chrome.storage.sync.get('CustomCSS', function(res) {
-  if (res['CustomCSS']) {
-    var saved = JSON.parse(res['CustomCSS']);
-    saved.map(function(item) {
-      console.log(item)
-      var li = document.createElement('li');
-      var h2 = document.createElement('h2');
-      var pre = document.createElement('pre');
-      h2.innerText = item.url;
-      pre.innerText = atob(item.CSS);
-      li.appendChild(h2);
-      li.appendChild(pre);
-      list.appendChild(li);
-    })
+function showStorage() {
+  chrome.storage.sync.get('CustomCSS', function(res) {
+    if (res['CustomCSS']) {
+      saved = JSON.parse(res['CustomCSS']);
+      saved.map(function(item) {
+        var li = document.createElement('li');
+        var h2 = document.createElement('h2');
+        var pre = document.createElement('pre');
+        var remove = document.createElement('button');
+        remove.innerText = 'X';
+        h2.innerText = item.url;
+        remove.setAttribute('data', item.url);
+        remove.addEventListener("click", removeItem);
+        pre.innerText = atob(item.CSS);
+        li.appendChild(h2);
+        li.appendChild(remove);
+        li.appendChild(pre);
+        list.appendChild(li);
+      })
+    }
+  });
+}
+
+showStorage();
+
+function removeItem(evt) {
+  var strg = [];
+  var url = evt.srcElement.attributes[0].nodeValue;
+  for (var i = 0; i < saved.length; i++) {
+    if (url == saved[i].url) {
+      console.log(saved[i])
+    } else {
+      strg.push(saved[i])
+    }
   }
-});
+  debugger
+  chrome.storage.sync.set({'CustomCSS': JSON.stringify(strg)}, function (res) {
+    console.log("saved", strg);
+    showStorage();
+  });
+}
